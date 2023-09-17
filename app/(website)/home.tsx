5639cc75c26de9602b6b1261a623c668446acec5
Reapimus <29674188+Reapimus@users.noreply.github.com>
2023-09-17 23:01:17 +1000

    Work on home page

--- app/(website)/home.tsx
+++ app/(website)/home.tsx
@@ -10,12 +10,23 @@ import {
   Image,
   createStyles,
   rem,
+  px,
 } from '@mantine/core';
+import isMobile from '@/helpers/isMobile';
+import { useEffect, useState } from 'react';
 
 const useStyles = createStyles((theme) => ({
     categoryList: {
-        height: rem(120),
+        height: rem(90),
         padding: rem(5),
+
+        [theme.fn.smallerThan('sm')]: {
+            height: 'auto',
+        },
+    },
+
+    categoryTitle: {
+        textShadow: '0 0 10px rgba(0, 0, 0, 0.8)',
     },
 
     categoryButtonImage: {
@@ -30,21 +41,6 @@ const useStyles = createStyles((theme) => ({
         '&:hover': {
             transform: 'scale(1.1)',
         },
-
-        '&:after': {
-            position: 'absolute',
-            content: '""',
-            height: '100%',
-            width: '100%',
-            top: 0,
-            backgroundImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.4) 75%)',
-            transition: 'transform .3s ease-in-out',
-            transform: 'translateY(100%)',
-        },
-
-        '&:hover:after': {
-            transform: 'translateY(0%)',
-        }
     }
 }));
 
@@ -52,20 +48,22 @@ function CategoryButton({
     name,
     image,
     href,
+    onMobile,
 }: {
     name: string;
     image: string;
     href: string;
+    onMobile: boolean;
 }) {
     const { classes } = useStyles();
 
     return (
-        <Anchor underline={false} h="100%" href={href}>
+        <Anchor underline={false} w={onMobile ? '100%' : 'auto'} h="100%" href={href}>
             <Paper h="100%" radius={4}>
                 <Center h="100%" pos={"relative"} sx={{overflow: 'hidden', borderRadius: 4, backdropFilter: 'blur(2px)'}}>
-                    <Image height={rem(110)} imageProps={{style: {objectPosition: '0 20%'}}} className={classes.categoryButtonImage} radius={4} src={image} />
+                    <Image height={rem(80)} imageProps={{style: {objectPosition: '0 20%'}}} className={classes.categoryButtonImage} radius={4} src={image} />
                     <Stack sx={{pointerEvents: 'none'}}>
-                        <Title order={1} color='white' sx={{pointerEvents: 'none'}}>{name}</Title>
+                        <Title className={classes.categoryTitle} order={1} color='white' sx={{pointerEvents: 'none'}}>{name}</Title>
                     </Stack>
                 </Center>
             </Paper>
@@ -75,13 +73,27 @@ function CategoryButton({
 
 export default function Home() {
     const { classes } = useStyles();
+    const [onMobile, setOM] = useState(false);
+    if (typeof window !== 'undefined') {
+        useEffect(() => {
+            setOM(isMobile());
+
+            const updateOM = () => {
+                setOM(isMobile());
+            }
+
+            window.addEventListener('resize', updateOM);
+
+            return () => window.removeEventListener('resize', updateOM);
+        }, []);
+    }
 
     return (
         <main>
-            <Group className={classes.categoryList} spacing={rem(5)} grow>
-                <CategoryButton name="Art" image="/categories/art.png" href="/art" />
-                <CategoryButton name="Comics" image="/categories/comics.png" href="/comics" />
-                <CategoryButton name="Novels" image="/categories/novels.png" href="/novels" />
+            <Group className={classes.categoryList} spacing={rem(5)} grow={!onMobile}>
+                <CategoryButton onMobile={onMobile} name="Art" image="/categories/art.png" href="/art" />
+                <CategoryButton onMobile={onMobile} name="Comics" image="/categories/comics.png" href="/comics" />
+                <CategoryButton onMobile={onMobile} name="Novels" image="/categories/novels.png" href="/novels" />
             </Group>
         </main>
     )
