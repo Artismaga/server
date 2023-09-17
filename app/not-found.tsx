fe405e3386e6afe942da5b306b44e2a8614d36a5
Reapimus <29674188+Reapimus@users.noreply.github.com>
2023-09-17 17:40:54 +1000

    Fix not found

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+import { NothingFound } from '@/app/error-pages';
+import { SiteHeader, SiteFooter } from './providers';
+
+export default function NotFound() {
+  return (
+    <>
+      <SiteHeader />
+      <div style={{height: 'calc(100vh - 60px - 66px)'}}>
+          <NothingFound />
+          <title>Not Found - Artismaga</title>
+      </div>
+      <SiteFooter />
+    </>
+  );
+}
